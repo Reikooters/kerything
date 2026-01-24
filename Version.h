832044ac9c9d99517d536ce018ac8e701da0640e
Reikooters <shane_m_swenson@hotmail.com>
2026-01-25 04:16:41 +1100

    Updated application version to v1.4.0

--- Version.h
+++ Version.h
@@ -7,7 +7,7 @@
 #include <string_view>
 
 namespace Version {
-    inline constexpr std::string_view VERSION = "1.3.5";
+    inline constexpr std::string_view VERSION = "1.4.0";
 };
 
 #endif //KERYTHING_VERSION_H
